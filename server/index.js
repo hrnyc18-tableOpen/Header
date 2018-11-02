176b520feeb608b5099b20af169e30c509fdb186
Nick <nicholas.a.eliades@gmail.com>
2018-11-02 19:55:16 -0400

    package json update

--- server/index.js
+++ server/index.js
@@ -85,7 +85,7 @@ app.post('/options', (req, res) => {
   res.send(list);
 });
 
-app.get('/*', (req, res) => {
+app.get('restaurants/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../client/dist/index.html'));
 });
 
